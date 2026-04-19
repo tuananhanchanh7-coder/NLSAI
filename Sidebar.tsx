7ba522cd757d334a3c0de3acfa88eb09532bc535
tuananhanchanh7-coder <tuananhanchanh7@gmail.com>
2026-04-19 08:29:14 +0700

    Add Sidebar component with basic layout

---
+++ Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Sidebar: React.FC = () => {
+  return (
+    <div style={{ width: "250px", background: "#f1f5f9", padding: "20px" }}>
+      <h2>Sidebar</h2>
+      <p>Menu bên trái</p>
+    </div>
+  );
+};
+
+export default Sidebar;
